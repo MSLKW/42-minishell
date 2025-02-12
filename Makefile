ac554c1dc3de4b477e819bcb3bd0bf054bb8cca1
MSLKW <maxwell.liew06@gmail.com>
2025-02-12 17:13:46 +0800

    Created project structure, files and folders

---
+++ Makefile
@@ -0,0 +1,38 @@
+NAME := minishell
+
+CC := cc
+CFLAGS := -Wall -Wextra -Werror -lreadline
+
+SRCS_DIR := ./srcs/
+SRCS_FILES :=	minishell.c \
+				
+DEPS_FILES :=	minishell.h
+
+SRCS = $(addprefix $(SRCS_DIR), $(SRCS_FILES))
+DEPS = $(addprefix $(SRCS_DIR), $(DEPS_FILES))
+OBJS = $(SRCS:.c=.o)
+
+LIBFT = libft/libft.a
+
+$(NAME): $(OBJS) $(DEPS) $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJS) $(LIBFT) -o $(NAME)
+
+$(LIBFT):
+	make -C ./libft
+
+%.o: %.c
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+.PHONY = all clean fclean re
+
+all: $(NAME)
+
+clean:
+	rm -f $(OBJS) $(BONUS_OBJS)
+	make -C ./libft clean
+
+fclean: clean
+	rm -f $(NAME)
+	make -C ./libft fclean
+
+re: fclean all
---
+++ srcs/minishell.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxliew <maxliew@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 16:51:32 by maxliew           #+#    #+#             */
+/*   Updated: 2025/02/12 16:51:35 by maxliew          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+// ===== Libraries =====
+
+// System Libraries ( Ref System Libraries Document for more information )
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <dirent.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <term.h>
+# include <fcntl.h>
+# include <string.h>
+# include <errno.h>
+
+// Custom Libraries
+# include "../libft/libft.h"
+
+// ===== Minishell Functions =====
+
+// 
+
+#endif
